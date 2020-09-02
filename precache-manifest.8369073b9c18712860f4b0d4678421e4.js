3add2abd3ff4bf3a3fce8831930f521373b9a5b6
Hasnain <our.hasnain22@gmail.com>
2020-09-03 00:19:06 +0500

    Updates

---
+++ precache-manifest.8369073b9c18712860f4b0d4678421e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4690121e00363a4e62cfdfdc6c8d0723",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "57d80eee78c615f006ff",
+    "url": "/robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fa8e8140fd3c80340cab",
+    "url": "/robofriend/static/css/main.0d982fd2.chunk.css"
+  },
+  {
+    "revision": "57d80eee78c615f006ff",
+    "url": "/robofriend/static/js/2.aa15af58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.aa15af58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8e8140fd3c80340cab",
+    "url": "/robofriend/static/js/main.4474afbd.chunk.js"
+  },
+  {
+    "revision": "b9549992ed9aa0bec102",
+    "url": "/robofriend/static/js/runtime-main.b099a5de.js"
+  }
+]);
